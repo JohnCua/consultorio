c8091b74ddb8ca9e8548f94bd00d17ad8e84bb35
JohnCua <cuatumax@gmail.com>
2017-11-21 20:01:11 -0600

    fin

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -18,10 +18,7 @@ class PacienteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-     public function __construct()
-     {
-         $this->middleware('auth');
-     }
+  
      public function __construct()
      {
          $this->middleware('auth');
